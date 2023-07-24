98f4b27712603e9fc9af8f34525b693747889a5f
veddo2 <43934088+veddo2@users.noreply.github.com>
2023-07-24 01:42:47 +0000

    foundation

--- Module/Sheets/itemsheet.js
+++ Module/Sheets/itemsheet.js
@@ -1,6 +1,6 @@
 export default class itemsheet extends itemsheet {
     get template() {
-        return 'systems/ieng/templates/sheets'
+        return 'systems/ieng/templates/sheets/${this.items.data.type}-sheets.html';
     }
 
 }
---
+++ ieng.js
@@ -0,0 +1,1 @@
+import itemsheet from "./Module/Sheets/itemsheet.js";
